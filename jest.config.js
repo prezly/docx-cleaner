cdab84f1d35afd0d07b37c930b4f9796ad143060
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-11-09 14:02:03 +0200

    Setup repo dev infrastructure

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.jest.json',
+        },
+    },
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['/node_modules/', '/build/'],
+};
