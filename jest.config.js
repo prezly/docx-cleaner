d1d0e1ad176efe242d5c4fa169c48503219fb70e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-30 19:37:54 +0200

    Configure dual ESM + CommonJS build

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
     globals: {
         'ts-jest': {
---
+++ mockDOMParser.js
@@ -0,0 +1,1 @@
+global.DOMParser = function () {};
